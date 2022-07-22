579b732ad66cd06e75054bc3dd1f4846602b3507
菜笔弱猴 <616505453@qq.com>
2022-07-22 15:42:27 +0800

    自定义扫描类

--- src/main/java/io/github/yangyouwang/core/config/ScanConfig.java
+++ src/main/java/io/github/yangyouwang/core/config/ScanConfig.java
@@ -1,6 +1,5 @@
 package io.github.yangyouwang.core.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
