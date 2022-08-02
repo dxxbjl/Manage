52f43937a938591b0d60666007de1adaa34f7d7f
菜笔弱猴 <616505453@qq.com>
2022-08-02 18:55:13 +0800

    优化字典代码

--- src/main/java/io/github/yangyouwang/crud/system/mapper/SysDictValueMapper.java
+++ src/main/java/io/github/yangyouwang/crud/system/mapper/SysDictValueMapper.java
@@ -2,19 +2,9 @@ package io.github.yangyouwang.crud.system.mapper;
 
 import io.github.yangyouwang.common.base.CrudBaseMapper;
 import io.github.yangyouwang.crud.system.entity.SysDictValue;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
-
 /**
  * 数据字典值Mapper
  * @author yangyouwang
  */
 public interface SysDictValueMapper extends CrudBaseMapper<SysDictValue> {
-    /**
-     * 批量插入字典项
-     * @param sysDictValues 字典项
-     * @return 新增或修改状态
-     */
-    int insertBatch(@Param("list")List<SysDictValue> sysDictValues);
 }
