85cbf72f2cb649363396af43b83dc3556d613224
菜笔弱猴 <616505453@qq.com>
2022-08-10 14:11:54 +0800

    优化代码

--- src/main/java/io/github/yangyouwang/crud/api/controller/ApiUserController.java
+++ src/main/java/io/github/yangyouwang/crud/api/controller/ApiUserController.java
@@ -2,8 +2,8 @@ package io.github.yangyouwang.crud.api.controller;
 
 import io.github.yangyouwang.common.annotation.ApiVersion;
 import io.github.yangyouwang.common.annotation.PassToken;
+import io.github.yangyouwang.common.annotation.ResponseResultBody;
 import io.github.yangyouwang.common.constant.ApiVersionConstant;
-import io.github.yangyouwang.common.domain.Result;
 import io.github.yangyouwang.crud.api.model.UserAuthDTO;
 import io.github.yangyouwang.crud.api.model.UserAuthVO;
 import io.github.yangyouwang.crud.api.service.ApiUserService;
@@ -31,6 +31,7 @@ import java.util.Objects;
 @RequestMapping("/api/{version}/user")
 @Api(tags = "ApiUserController", description = "用户相关接口")
 @RequiredArgsConstructor
+@ResponseResultBody
 public class ApiUserController {
 
     private final ApiUserService apiUserService;
@@ -43,11 +44,10 @@ public class ApiUserController {
     @PostMapping("/user_auth")
     @ApiOperation(value="用户授权", notes="用户授权")
     @PassToken
-    public Result userAuth(@Valid @RequestBody UserAuthDTO userAuthDTO, BindingResult bindingResult) {
+    public UserAuthVO userAuth(@Valid @RequestBody UserAuthDTO userAuthDTO, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return Result.failure(Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage());
+            throw new RuntimeException(Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage());
         }
-        UserAuthVO result = apiUserService.userAuth(userAuthDTO);
-        return Result.success(result);
+        return apiUserService.userAuth(userAuthDTO);
     }
 }
