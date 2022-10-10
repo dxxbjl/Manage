0f80380d71cf0158613f7cbe70c384c1267aae30
菜笔弱猴 <616505453@qq.com>
2022-10-10 20:35:43 +0800

    升级layui-v2.5.6版本

--- src/main/resources/static/layuiadmin/config.js
+++ src/main/resources/static/layuiadmin/config.js
@@ -4,9 +4,9 @@
  @Author：贤心
  @Site：http://www.layui.com/admin/
  @License：LPPL（layui付费产品协议）
-    
+
  */
- 
+
 layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
   exports('setter', {
     container: 'LAY_app' //容器ID
@@ -15,18 +15,18 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
     ,entry: 'index' //默认视图文件名
     ,engine: '.html' //视图文件后缀名
     ,pageTabs: true //是否开启页面选项卡功能。iframe版推荐开启
-    
+
     ,name: 'layuiAdmin'
     ,tableName: 'layuiAdmin' //本地存储表名
     ,MOD_NAME: 'admin' //模块事件名
-    
+
     ,debug: true //是否开启调试模式。如开启，接口异常时会抛出异常 URL 等信息
 
     //自定义请求字段
     ,request: {
       tokenName: false //自动携带 token 的字段名（如：access_token）。可设置 false 不携带。
     }
-    
+
     //自定义响应字段
     ,response: {
       statusName: 'code' //数据状态的字段名称
@@ -37,13 +37,13 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
       ,msgName: 'msg' //状态信息的字段名称
       ,dataName: 'data' //数据详情的字段名称
     }
-    
+
     //扩展的第三方模块
     ,extend: [
       'echarts', //echarts 核心包
       'echartsTheme' //echarts 主题
     ]
-    
+
     //主题配置
     ,theme: {
       //内置主题配色方案
@@ -101,8 +101,26 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
       },{
         header: '#393D49'
         ,alias: 'classic-black-header' //经典黑头
+      },{
+        main: '#50314F'
+        ,logo: '#50314F'
+        ,selected: '#7A4D7B'
+        ,header: '#50314F'
+        ,alias: 'purple-red-header' //紫红头
+      },{
+        main: '#28333E'
+        ,logo: '#28333E'
+        ,selected: '#AA3130'
+        ,header: '#AA3130'
+        ,alias: 'fashion-red-header' //时尚红头
+      },{
+        main: '#28333E'
+        ,logo: '#009688'
+        ,selected: '#009688'
+        ,header: '#009688'
+        ,alias: 'green-header' //墨绿头
       }]
-      
+
       //初始的颜色索引，对应上面的配色方案数组索引
       //如果本地已经有主题色记录，则以本地记录为优先，除非请求本地数据（localStorage）
       ,initColorIndex: 0
