956d902bda8e97d6388463f8b61282245d6fbc9d
yangyouwang <12345678>
2022-11-18 10:40:44 +0800

    发起流程

--- src/main/java/io/github/yangyouwang/crud/act/model/StartDTO.java
+++ src/main/java/io/github/yangyouwang/crud/act/model/StartDTO.java
@@ -3,7 +3,7 @@ package io.github.yangyouwang.crud.act.model;
 import lombok.Data;
 
 /**
- * Description: 启动流程<br/>
+ * Description: 发起流程<br/>
  * date: 2022/11/17 23:13<br/>
  *
  * @author yangyouwang<br />
